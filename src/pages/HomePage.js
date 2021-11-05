270fea69cd1bce0634e1b0314865c2471eeabf4e
melissikiss <melissikiss@gmail.com>
2021-11-04 20:56:35 -0700

    added homepage image and invite to book appt

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Fade from 'react-reveal/Fade';
+import { Link } from 'react-router-dom';
 
 import placeholderIMG1 from '../assets/images/makeup-pallete.jpg'
 function HomePage() {
     return (
         <main id="homepage">
             <div className="container">
-                <img src={placeholderIMG1} className="homepage-init-img"/>
+                <div className="row mt-5">
+                    <div className="col-md-6 d-flex justify-content-end">
+                        <Fade left>
+                            <img src={placeholderIMG1} className="homepage-init-img" />
+                        </Fade>
+                    </div>
+                    <Fade top>
+                        <div className="col-md-6">
+                            <div className="overlap-text-div">
+                                <h1 className="overlap-h1">BOOK TODAY</h1>
+                                <p className="overlap-p">Check out my availability and schedule an appointment with me today!</p>
+                                <button className="overlap-btn"><Link to={'/appointment'}>LET'S GO</Link></button>
+                            </div>
+                        </div>
+                    </Fade>
+                </div>
             </div>
         </main>
     )
