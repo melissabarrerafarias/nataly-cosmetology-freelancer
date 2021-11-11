5c787d1969b75d27c6289f976eb0b50d9bc9a3e1
melissikiss <melissikiss@gmail.com>
2021-11-10 18:33:42 -0800

    added columns for services and invite to book appointment

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -9,7 +9,7 @@ import prettyGirl from '../assets/images/pretty.jpg';
 function Services() {
     return (
         <main id="services">
-            <div className="">
+            <div className="services-top">
                 <div className="row">
                     <div className="col-md-6 you-look-good text-center">
                         <Pulse>
@@ -22,6 +22,21 @@ function Services() {
                         <Slide right>
                             <img src={prettyGirl} />
                         </Slide>
+                    </div>
+                </div>
+                <hr></hr>
+            </div>
+            
+            <div>
+                <div className="row">
+                    <div className="col-md-6">
+                        <a>Hair</a>
+                        <a>Makeup</a>
+                        <a>Group Events</a>
+                    </div>
+                    <div className="col-md-6">
+                        <p>Already know what you want done?</p>
+                        <p>Schedule an appointment!</p>
                     </div>
                 </div>
             </div>
